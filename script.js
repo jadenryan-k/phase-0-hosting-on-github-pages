75f222c18395b70814fdd24d3d360ad95a4e34fe
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-06 02:44:56 +0300

    add style.css, script.js,  connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "The Hellcat";
 document.querySelector("body").appendChild(h2);
+document.getElementsByName("The Hellcat").style.textAlign = "center";
