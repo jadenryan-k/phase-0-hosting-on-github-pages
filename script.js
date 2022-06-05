3d9e939b5d399996e23f94e67ae770dbf47038ed
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-06 02:47:25 +0300

    Done.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "The Hellcat";
+h2.textContent = "For the love of American muscle";
 document.querySelector("body").appendChild(h2);
-document.getElementsByName("The Hellcat").style.textAlign = "center";
+document.getElementsByName("For the love for American muscle").style.textAlign = "center";
